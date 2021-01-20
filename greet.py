7c48086997f6e0171e4272d36c4702888744b187
Cyberspaze <alexb@student.chalmers.se>
2021-01-20 12:55:48 +0100

    made greet() func

--- greet.py
+++ greet.py
@@ -1,9 +1,21 @@
 from random import choice
 
 def greet():
-    with open('words.txt') as file:
+    with open('words.txt', encoding='utf8') as file:
         words = file.readlines()
 
     word = choice(words)
+    word = word.strip()
+    word = word.capitalize()
+    if (word[-1] == 'd'):
+        word = word[:-1] + 't'
+    elif (not(isVowel(word[-1])) and word[-1] != 't'):
+        word += 't'
+
+    phrase = word + " välkommen!"
+    return phrase
+
+def isVowel(c):
+    isVowel = (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u')
+    return isVowel
     
-    return f"{word}  välkommen!"
